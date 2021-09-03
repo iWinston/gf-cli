d404c4aa47140d0205e7efdc0fef986ac44a2d01
jinhao <jinhao@sevenfifteen.onaliyun.com>
2021-09-03 11:32:08 +0800

    fix: new api template

--- commands/gen/gen_api_template.go
+++ commands/gen/gen_api_template.go
@@ -14,67 +14,75 @@ import (
 
 // {{$.Description}}API
 type {{$.CamelPrefix}}{{$.CamelName}}Api struct{}
-
 {{- range $method := .Methods}}
-{{- if eq $method "get" "post" "patch" "delete" "list"}}
-// @summary 【{{Title $method }}】{{if $.CamelPrefix}}{{$.CamelPrefix}}/{{end}}{{$.Description}}
-// @description 新增{{$.Description}}
+{{- if eq $method "post" "patch" "delete"}}
+// @summary 【{{Title $method}}】{{if $.CamelPrefix}}{{$.CamelPrefix}}/{{end}}{{$.Description}}
 // @tags    {{$.SystemName}}/{{$.Description}}管理
 // @produce  json
 // @security ApiKeyAuth
-// @param   entity  body define.{{$.CamelName}}{{Title $method}}Param true "新增{{$.Description}}"
-// @router  {{if $.Prefix}}/{{$.Prefix}}{{end}}/{{$.SystemName}}/{{$.Name}}/{{$method}} [Post]
-{{- if eq $method "post"}}
+// @param   entity  body define.{{$.CamelName}}{{Title $method}}Param true "{{- if eq $method "post"}}"新增{{$.Description}}{{- else}}{{end}}{{- if eq $method "patch"}} "修改{{$.Description}}{{- else}}{{end}}{{- if eq $method "delete"}} "删除{{$.Description}}{{- else}}{{end}}"
+// @router  {{- if $.Prefix}}/{{$.Prefix}}{{- end}}/{{$.SystemName}}/{{$.Name}}/{{$method}} [Post]
 // @success 200 {object} q.JsonResponse "执行结果"
-func (a *{{$.CamelPrefix}}{{$.CamelName}}Api) Post(r *ghttp.Request) {
-	ctx := qservice.ReqContext.Get(r.Context())
-	ctx.SetActionHistoryTypeAndDesc("{{$.Description}}管理", "新增{{$.Description}}")
-	param := &define.{{$.CamelName}}PostParam{}
+func (a *{{- if $.Prefix}}{{$.Prefix}}{{$.CamelName}}{{- else}}{{$.CamelName}}{{- end}}Api) {{Title $method}}(r *ghttp.Request) {
+	var (
+		ctx   = qservice.ReqContext.Get(r.Context())
+		param = &define.{{$.CamelName}}{{Title $method}}Param{}
+		tx    = ctx.TX.Model(&model.{{Title $.SystemName}}{{$.CamelName}}{}).WithContext(r.Context())
+	)
+
+	ctx.SetActionHistoryTypeAndDesc("{{$.Description}}管理", {{- if eq $method "post"}} "新增{{$.Description}}{{- else}}{{end}}{{- if eq $method "patch"}} "修改{{$.Description}}{{- else}}{{end}}{{- if eq $method "delete"}}"删除{{$.Description}}{{- else}}{{end}}")
 	q.AssignParamFormReq(r, param)
-	err := service.{{$.CamelName}}.Post(ctx, param)
+	err := q.{{Title $method}}(tx, param)
 	q.Response(r, err)
 }
+
 {{- else if eq $method "get"}}
-// @success 200 {object} q.JsonResponseWithData{data=define.{{$.CamelName}}GetRes} "执行结果"
-func (a *{{$.CamelPrefix}}{{$.CamelName}}Api) Get(r *ghttp.Request) {
-	ctx := qservice.ReqContext.Get(r.Context())
-	param := &define.{{$.CamelName}}GetParam{}
+// @summary 【{{$method}}】{{if $.CamelPrefix}}{{$.CamelPrefix}}/{{end}}{{$.Description}}
+// @description 获取{{$.Description}}
+// @tags    {{$.SystemName}}/{{$.Description}}管理
+// @produce  json
+// @security ApiKeyAuth
+// @param   entity  body define.{{$.CamelName}}{{$method}}Param true "新增{{$.Description}}"
+// @router  {{- if $.Prefix}}/{{$.Prefix}}{{- end}}/{{$.SystemName}}/{{$.Name}}/{{$method}} [{{Title $method}}]
+// @success 200 {object} q.JsonResponseWithData{data=define.{{$.CamelName}}{{$method}}Res} "执行结果"
+func (a *{{- if $.Prefix}}{{$.Prefix}}{{$.CamelName}}{{- else}}{{$.CamelName}}{{- end}}Api) {{Title $method}}(r *ghttp.Request) {
+	var (
+		ctx   = qservice.ReqContext.{{Title $method}}(r.Context())
+		param = &define.{{$.CamelName}}{{Title $method}}Param{}
+		res   = &define.{{$.CamelName}}{{Title $method}}Res{}
+		tx    = ctx.DB.Model(&model.{{Title $.SystemName }}{{$.CamelName}}{}).WithContext(r.Context())
+	)
+
 	q.AssignParamFormReq(r, param)
-	res, err := service.{{$.CamelName}}.Get(ctx, param)
+	err := q.{{Title $method}}(tx, param, res)
 	q.ResponseWithData(r, err, res)
 }
-{{- else if eq $method "patch"}}
-// @success 200 {object} q.JsonResponse "执行结果"
-func (a *{{$.CamelPrefix}}{{$.CamelName}}Api) Patch(r *ghttp.Request) {
-	ctx := qservice.ReqContext.Get(r.Context())
-	ctx.SetActionHistoryTypeAndDesc("{{$.Description}}管理", "修改{{$.Description}}")
-	param := &define.{{$.CamelName}}PatchParam{}
-	q.AssignParamFormReq(r, param)
-	err := service.{{$.CamelName}}.Patch(ctx, param)
-	q.Response(r, err)
-}
-{{- else if eq $method "delete"}}
-// @success 200 {object} q.JsonResponse "执行结果"
-func (a *{{$.CamelPrefix}}{{$.CamelName}}Api) Delete(r *ghttp.Request) {
-	ctx := qservice.ReqContext.Get(r.Context())
-	ctx.SetActionHistoryTypeAndDesc("{{$.Description}}管理", "删除{{$.Description}}")
-	param := &define.{{$.CamelName}}DeleteParam{}
-	q.AssignParamFormReq(r, param)
-	err := service.{{$.CamelName}}.Delete(ctx, param)
-	q.Response(r, err)
-}
+
 {{- else if eq $method "list"}}
+// @summary 【List】{{$.Description}}
+// @description {{$.Description}}列表
+// @tags    {{$.SystemName}}/{{$.Description}}管理
+// @produce  json
+// @security ApiKeyAuth
+// @param   entity  body define.{{$.CamelName}}ListParam true "{{$.Description}}列表"
+// @router  {{- if $.Prefix}}/{{$.Prefix}}{{- end}}/{{$.SystemName}}/{{$.Name}}/list [Post]
 // @success 200 {object} q.JsonResponseWithMeta{data=[]define.{{$.CamelName}}ListRes} "执行结果"
-func (a *{{$.CamelPrefix}}{{$.CamelName}}Api) List(r *ghttp.Request) {
-	ctx := qservice.ReqContext.Get(r.Context())
-	param := &define.{{$.CamelName}}ListParam{}
+func (a *{{- if $.Prefix}}{{$.Prefix}}{{$.CamelName}}{{- else}}{{$.CamelName}}{{- end}}Api) List(r *ghttp.Request) {
+	var (
+		ctx         = qservice.ReqContext.{{Title $method}}(r.Context())
+		param       = &define.{{$.CamelName}}ListParam{}
+		res         = &define.{{$.CamelName}}ListRes{}
+		tx          = ctx.DB.Model(&model.{{Title $.SystemName }}{{$.CamelName}}{}).WithContext(r.Context())
+		total int64 = 0
+	)
+
 	q.AssignParamFormReq(r, param)
-	res, total, err := service.{{$.CamelName}}.List(ctx, param)
+	err := q.List(tx, param, res, &total)
 	q.ResponseWithMeta(r, err, res, total)
 }
-{{end}}
-{{end}}
-{{end}}`
+{{else}}
+{{- end}}
+{{- end}}`
 
 var apiIndexTemplate = `package api
 
